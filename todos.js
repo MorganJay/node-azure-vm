ab2cc26be6c7cfdd8fa84731e2182d0c879719ee
Morgan Jay <jetmorgan.jm@gmail.com>
2023-05-18 11:17:52 +0100

    add more todos to test deployment

--- todos.js
+++ todos.js
@@ -1,14 +1,22 @@
 module.exports = [
   {
     id: 1,
-    task: "Fix Sink",
+    task: 'Fix Sink',
   },
   {
     id: 2,
-    task: "Buy Groceries",
+    task: 'Buy Groceries',
   },
   {
     id: 3,
-    task: "Wash the dishes",
+    task: 'Wash the dishes',
+  },
+  {
+    id: 4,
+    task: 'Make Dinner',
+  },
+  {
+    id: 5,
+    task: 'Take out the trash',
   },
 ];
